cb26c6be8ea1ecc7bd756e884904618739f7640e
Lucas Biz Teixeira <lucaasbizteixeira@gmail.com>
2022-08-19 11:12:29 -0300

    novo exercicio

--- aula14ex/modelo/ex16/script.js
+++ aula14ex/modelo/ex16/script.js
@@ -5,10 +5,29 @@ var res = document.getElementById('res')
 
 
 function contagem(){
-    if (ini.value.length < 1 || fim.value.length < 1 || pas.value.length < 1){window.alert('[ERRO] Todos os campos devem ser preenchidos!')} else if (pas.value == 0){window.alert('[ERRO]O valor de passos não pode ser 0!')} else{
-        var cont = ''
-        while(cont <= fim){
-            cont = Number(ini.value) + Number(pas.value)
+    
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0){window.alert('[ERRO] Todos os campos devem ser preenchidos!')} else if (pas.value <= 0){window.alert('[ERRO]O valor de passos não pode ser 0 ou negativo!')} else{
+
+        res.innerHTML = 'Contando: '
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(pas.value)
+
+        if (i<=f){
+
+            while(i <= f){
+
+                res.innerHTML += `${i} > `
+                i = i + p
+            }
+        } else
+            while(f <= i){
+                res.innerHTML += `${i} > `
+                i = i - p
+            }
+        
         }
+        res.innerHTML += 'Fim!'
+
     }
-}
+
